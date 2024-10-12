e6cc97a45cf9e7ab7d279642a30e617ef3d2700e
munaray <saabdulsalam1215225@gmail.com>
2024-10-12 17:45:28 +0100

    Implementing header.tsx

---
+++ src/constants/index.ts
@@ -0,0 +1,35 @@
+export const TOP_NAVs = [
+	{
+		id: 1,
+		name: "Home",
+		href: "#home",
+	},
+	{
+		id: 2,
+		name: "About",
+		href: "#about",
+	},
+	{
+		id: 3,
+		name: "Work",
+		href: "#work",
+	},
+];
+
+export const SIDE_NAVs = [
+	{
+		id: 1,
+		name: "Home",
+		href: "#home",
+	},
+	{
+		id: 2,
+		name: "About",
+		href: "#about",
+	},
+	{
+		id: 3,
+		name: "Work",
+		href: "#work",
+	},
+];
